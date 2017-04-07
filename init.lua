a0cddc3b30cdacd50270ee47bdf09da5a6c4a1a0
Matthias Berla <matthias@berla.net>
2017-04-07 17:58:05 +0200

    Put properties on client instead of additional field

--- init.lua
+++ init.lua
@@ -1,92 +1,81 @@
 local awful = require("awful")
-local screen = screen
 local client = client
 local tag = tag
-local mouse = mouse
 local ipairs = ipairs
 
 module("awesome-remember-geometry")
 
 -- {{{ Remember client size when switching between floating and tiling.
-local floatingwindows = {}
-
-tag.connect_signal("property::layout", function(t)
-	for k, c in ipairs(t:clients()) do
-		if floatingwindows[c.window] and isFloatingClient(c) then
-			c:geometry(floatingwindows[c.window].geometry)
-		end
-	end
-end)
-
 client.connect_signal("maximize", function(c)
-if not floatingwindows[c.window] then
-	floatingwindows[c.window] = {}
-end
--- max if
---- both are false
---- only one is true
-local max_h = c.maximized_horizontal
-local max_v = c.maximized_vertical
-local max = (not max_h and not max_v) or not (max_h and max_v)
-floatingwindows[c.window].manual_max = max
-c.maximized_horizontal = max
-c.maximized_vertical = max
-end)
-
-client.connect_signal("unmanage", function(c) 
-floatingwindows[c.window] = nil
+	-- max if
+	--- both are false
+	--- only one is true
+	local max_h = c.maximized_horizontal
+	local max_v = c.maximized_vertical
+	local max = (not max_h and not max_v) or not (max_h and max_v)
+	c.remember_geometry.maximized_manual = max
+	c.maximized_horizontal = max
+	c.maximized_vertical = max
 end)
 
 client.connect_signal("manage", function(c)
-if isFloatingClient(c) and c.minimized == false then
-	floatingwindows[c.window]={geometry=c:geometry()}
+c.remember_geometry = {
+	floating_geometry=c:geometry(),
+	maximized_manual=false,
+	maximized_horizontal=c.maximized_horizontal,
+	maximized_vertical=c.maximized_vertical
+}
+end)
+
+client.connect_signal("unmanage", function(c)
+c.remember_geometry = nil
+end)
+
+tag.connect_signal("property::layout", function(t)
+if t.layout == awful.layout.suit.floating then
+	for k, c in ipairs(t:clients()) do
+		c:geometry(c.remember_geometry.floating_geometry)
+	end
 end
 end)
 
-client.connect_signal("property::geometry", function(c)
-if isFloatingClient(c) and c.fullscreen == false and c.minimized == false then
-	if not floatingwindows[c.window] then
-		floatingwindows[c.window] = {}
-	end
+client.connect_signal("property::floating_geometry", function(c)
+if not c.fullscreen and not c.minimized then
 	-- if client is almost maximized then set it to maximized.
 	-- if client was maximized before allow to go back to normal view.
 	cgeometry = c:geometry()
 	sgeometry = screen[c.screen].workarea
-	floatingwindows[c.window].geometry = cgeometry
-	if c.maximized_horizontal == false then
+	c.remember_geometry.floating_geometry = cgeometry
+	if not c.maximized_horizontal then
 		diffWidth = sgeometry.width - cgeometry.width - c.border_width
 		xpos = sgeometry.x - cgeometry.x
-		if not floatingwindows[c.window].auto_max_width and diffWidth == 0 and xpos == 0 then
-			floatingwindows[c.window].auto_max_width = true
+		if not c.remember_geometry.maximized_horizontal and diffWidth == 0 and xpos == 0 then
+			c.remember_geometry.maximized_horizontal = true
 			c.maximized_horizontal = true
 		else
-			floatingwindows[c.window].auto_max_width = false
+			c.remember_geometry.maximized_horizontal = false
 		end
 	end
-	if c.maximized_vertical == false then
+	if not c.maximized_vertical then
 		diffHeight = sgeometry.height - cgeometry.height - c.border_width
 		ypos = sgeometry.y - cgeometry.y
-		if not floatingwindows[c.window].auto_max_height and diffHeight == 0 and ypos == 0 then
-			floatingwindows[c.window].auto_max_height = true
+		if not c.remember_geometry.maximized_vertical and diffHeight == 0 and ypos == 0 then
+			c.remember_geometry.maximized_vertical = true
 			c.maximized_vertical = true
 		else
-			floatingwindows[c.window].auto_max_height = false
+			c.remember_geometry.maximized_vertical = false
 		end
 	end
 end
 end)
 
 client.connect_signal("property::fullscreen", function(c)
-if isFloatingClient(c) and floatingwindows[c.window] and c.fullscreen == false then
-	c:geometry(floatingwindows[c.window].geometry)
-	if floatingwindows[c.window].manual_max then
+if c.floating and not c.fullscreen then
+	c:geometry(c.remember_geometry.floating_geometry)
+	if c.remember_geometry.maximized_manual then
 		c.maximized_horizontal = true
 		c.maximized_vertical = true
 	end
 end
 end)
-
-function isFloatingClient(c)
-	return (awful.layout.get(mouse.screen) == awful.layout.suit.floating) or (c.floating == true)
-end
 -- }}}
